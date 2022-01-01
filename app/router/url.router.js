151e8aff2b1f21f138eda7a23c1d2ddf8bcad44c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 11:02:35 +0800

    Delete app directory

--- app/router/url.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const UrlController = require('../controllers/url.controller');
-
-router.post('/', UrlController.insertUrl)
-router.get('/:url', UrlController.redirectUrl)
-
-module.exports = router;
